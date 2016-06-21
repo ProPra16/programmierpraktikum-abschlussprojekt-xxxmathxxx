7b4c2aee5ff8ba657f6cb9f3af98f40392207dfb
Fabi <fweiss96@googlemail.com>
2016-06-21 14:21:28 +0200

    Testcommit
    Ist das alles so richtig? Wird mir leider in Eclipse nicht als Java-Projekt angezeigt, gibt dem entsprechend Probleme... Habt ihr das auch?

---
+++ tddt/tddt/src/test/java/org/xxxmathxxx/tddt/data/ExerciseTests.java
@@ -0,0 +1,12 @@
+package org.xxxmathxxx.tddt.data;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ExerciseTests {
+    @Test public void constructorTest() {
+        new Exercise();
+        assertEquals(true, true);
+    }
+}
