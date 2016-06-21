629f8260771582996449883e7420b8ea80c28efd
Fourier <Philipp.Spohr@hhu.de>
2016-06-21 12:34:56 +0200

    More example for LaTex-N00bies

--- tddt/tddt/src/main/java/org/xxxmathxxx/tddt/babysteps/TaskTimer.java
+++ tddt/tddt/src/main/java/org/xxxmathxxx/tddt/babysteps/TaskTimer.java
@@ -3,11 +3,24 @@ package org.xxxmathxxx.tddt.babysteps;
 import java.util.Timer;
 import java.util.TimerTask;
 
+/** An abstract class providing a simple timer, useful for tracking time in the babysteps context
+ * @author xxxMathxxx, Jun 21, 2016
+ *
+ */
 public class TaskTimer {
 	
+	/**
+	 * The internal timer object
+	 */
 	private Timer timer;
+	/**
+	 * The elapsed time in seconds
+	 */
 	private int elapsedTime; 
 	
+	/**
+	 * Default constructor, creates a new timer that is instantly running
+	 */
 	public TaskTimer(){
 		timer = new Timer();
 		timer.scheduleAtFixedRate(new TimeUpdateTask(),0 , 1000);
@@ -20,6 +33,9 @@ public class TaskTimer {
 		}
 	}
 	
+	/**
+	 * @return
+	 */
 	public String getTimeInSecondsAsString(){
 		return ""+elapsedTime;
 	}
