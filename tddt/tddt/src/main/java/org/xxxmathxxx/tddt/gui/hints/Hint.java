02a508404e62e625b65a3455e0a9b701b553616e
Fourier <Philipp.Spohr@hhu.de>
2016-06-22 10:41:38 +0200

    Too much time before Bio-Evolution class ...

---
+++ tddt/tddt/src/main/java/org/xxxmathxxx/tddt/gui/hints/Hint.java
@@ -0,0 +1,10 @@
+package org.xxxmathxxx.tddt.gui.hints;
+
+public class Hint {
+	String imagePath;
+	String infoText;
+	public Hint(String imagePath, String infoText){
+		this.imagePath = imagePath;
+		this.infoText = infoText;
+	}
+}
