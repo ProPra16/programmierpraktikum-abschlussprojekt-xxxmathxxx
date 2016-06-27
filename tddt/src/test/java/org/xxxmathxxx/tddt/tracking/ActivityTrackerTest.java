07d31949ffb67537a0f78659dc8e485ac65af572
Fourier <Philipp.Spohr@hhu.de>
2016-06-27 13:09:00 +0200

    Removed non-required pre-built docs

--- tddt/src/test/java/org/xxxmathxxx/tddt/tracking/ActivityTrackerTest.java
+++ tddt/src/test/java/org/xxxmathxxx/tddt/tracking/ActivityTrackerTest.java
@@ -5,7 +5,7 @@ package org.xxxmathxxx.tddt.tracking;
 
 /**
  * @author Tschebyscheff, 23.06.16
- *  no tests necessary
+ *  no tests necessary ->is this file nesc then?
  *
  */
 public class ActivityTrackerTest {
