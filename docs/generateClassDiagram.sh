3358bbe4fb758b95c34c7e8d5f88519526050d5f
Fourier <Philipp.Spohr@hhu.de>
2016-06-28 13:15:08 +0200

    Improved doc scripts

--- docs/generateClassDiagram.sh
+++ docs/generateClassDiagram.sh
@@ -1,5 +1,14 @@
+#Check dependencies
+if ! type "java" > /dev/null; then
+	read -p "It appears that java is not installed on your system!" -n1 -s
+	exit 0
+fi
+if ! type "pdflatex" > /dev/null; then
+	read -p "It appears that LaTex/pdflatex is not installed on your system!" -n1 -s
+	exit 0
+fi
 #Generates an up-to-date class diagram
-java -jar ../tddt/external/Heretikz.jar ../tddt/src classDiagram.tex
+java -jar ../tddt/external/Heretikz.jar ../tddt/src/main classDiagram.tex
 #Compiles the .tex files into pdf
 pdflatex -jobname=class-diagram diagramSkeleton.tex
 #Clean-up temporary files
--- docs/generateJavaDoc.sh
+++ docs/generateJavaDoc.sh
@@ -1,3 +1,8 @@
+#Check dependencies
+if ! type "gradle" > /dev/null; then
+	read -p "It appears that gradle is not installed on your system!" -n1 -s
+	exit 0
+fi
 #Generates an up-to-date javadoc
 cd ../tddt
 gradle javadoc
