523c3c74b8d3e78df03be35e3751d179b1416ef7
Fabi <fweiss96@googlemail.com>
2016-06-28 19:09:28 +0200

    fugit

--- tddt/src/main/java/org/xxxmathxxx/tddt/io/ExerciseReader.java
+++ tddt/src/main/java/org/xxxmathxxx/tddt/io/ExerciseReader.java
@@ -1,12 +1,15 @@
 package org.xxxmathxxx.tddt.io;
 
 import java.io.File;
+
 import javax.xml.parsers.*;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
+import org.xxxmathxxx.tddt.data.ClassData;
 import org.xxxmathxxx.tddt.data.Exercise;
 import org.xxxmathxxx.tddt.data.ExerciseClass;
+import org.xxxmathxxx.tddt.data.ExerciseConfig;
 import org.xxxmathxxx.tddt.data.ExerciseTest;
 import org.xxxmathxxx.tddt.data.JavaCode;
 import org.xxxmathxxx.tddt.logging.TDDTLogManager;
@@ -17,54 +20,56 @@ public class ExerciseReader {
 	DocumentBuilder builder;
 	TDDTLogManager logger; //Huehue like woodcutting
 
+	/**
+	 * Creates new ExerciseReader. You have to initialise this class!
+	 */
 	public ExerciseReader() {
 		try {
 			factory = DocumentBuilderFactory.newInstance();
 			builder = factory.newDocumentBuilder();
-			
-			
-			logger= TDDTLogManager.getInstance();
-			
+
+			logger = TDDTLogManager.getInstance();
+
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
-	public Exercise readExercise(String filename)
-	{
-		File input = new File("exercises/"+filename);
+
+	/**
+	 * Reads Exercise from Disc
+	 * 
+	 * @param filename
+	 *            Filename with fileextension e.g.: "RomanNumbers.xml"
+	 * @return returns Exercise as Exercise Class
+	 */
+	public Exercise readExercise(String filename) {
+		File input = new File("exercises/" + filename);
 		Document exercise;
 		NodeList pointer;
-		
+
 		try {
 			exercise = builder.parse(input);
+
 		} catch (Exception e) 
 		{
 			TDDTLogManager.getInstance().logMessage("You've screwed up m8. Likely the file you're searching for doesn't exist or is not in xml form.");
+
+
 			return null;
 		}
-		
-		//Creating variables for the exercise object
+
+		// Creating variables for the exercise object
 		String name;
 		String id;
 		String description;
+
 		ExerciseClass[] referencedClasses;
 		ExerciseTest[] referencedTests;
+		ExerciseTest[] referencedFinishTests;
 
-		//Boolean babysteps;
-		//Boolean timetracking;
-		
-		//TODO: Import Medal timings
-		
-//->Some missing
-		
-		//All variables created
-		
-		
-		
 		//Fetches name
-		 pointer= exercise.getElementsByTagName("name");
+		pointer= exercise.getElementsByTagName("name");
 		
 		name=pointer.item(0).getTextContent();
 		
@@ -77,35 +82,72 @@ public class ExerciseReader {
 		pointer= exercise.getElementsByTagName("description");
 		
 		description=pointer.item(0).getTextContent();
-		
-		//Fetches Classes
-		pointer= exercise.getElementsByTagName("class");
-		referencedClasses= new ExerciseClass[pointer.getLength()];
-		
-		for(int i=0; i<pointer.getLength();i++)
-		{
-			String className=pointer.item(i).getAttributes().getNamedItem("name").getTextContent();
-			String classCode= pointer.item(i).getTextContent();
-			
-			referencedClasses[i]= new ExerciseClass(className, new JavaCode(classCode));
+
+		// Fetches Classes
+		pointer = exercise.getElementsByTagName("class");
+		referencedClasses = new ExerciseClass[pointer.getLength()];
+
+		for (int i1 = 0; i1 < pointer.getLength(); i1++) {
+			String className1 = pointer.item(i1).getAttributes().getNamedItem("name").getTextContent();
+			String classCode1 = pointer.item(i1).getTextContent();
+
+			referencedClasses[i1] = new ExerciseClass(className1, new JavaCode(classCode1));
+
 		}
-		
-		//Fetches Tests
-		pointer= exercise.getElementsByTagName("test");
-		referencedTests= new ExerciseTest[pointer.getLength()];
-		
-		for(int i=0; i<pointer.getLength();i++)
-		{
-			String testName=pointer.item(i).getAttributes().getNamedItem("name").getTextContent();
-			String testCode= pointer.item(i).getTextContent();
-			
-			referencedTests[i]= new ExerciseTest(testName, new JavaCode(testCode));
+
+		// Fetches Tests
+		pointer = exercise.getElementsByTagName("test");
+		referencedTests = new ExerciseTest[pointer.getLength()];
+
+		for (int i1 = 0; i1 < pointer.getLength(); i1++) {
+			String testName = pointer.item(i1).getAttributes().getNamedItem("name").getTextContent();
+			String testCode = pointer.item(i1).getTextContent();
+
+			referencedTests[i1] = new ExerciseTest(testName, new JavaCode(testCode));
 		}
-		
-		
-		return new Exercise(name, id, description, referencedClasses, referencedTests); //Test
+
+		// Fetches Finishtest ->Extension
+		pointer = exercise.getElementsByTagName("finishtest");
+		referencedFinishTests = new ExerciseTest[pointer.getLength()];
+
+		for (int i = 0; i < pointer.getLength(); i++) {
+			String testName = pointer.item(i).getAttributes().getNamedItem("name").getTextContent();
+			String testCode = pointer.item(i).getTextContent();
+
+			referencedFinishTests[i] = new ExerciseTest(testName, new JavaCode(testCode));
+		}
+
+		// ****Fetches config
+
+		// Babysteps
+		pointer = exercise.getElementsByTagName("babysteps");
+
+		Boolean babysteps = pointer.item(0).getAttributes().getNamedItem("value").getTextContent().equals("True"); // Babysteps
+																													// enabled
+																													// BTW:
+																													// THIS
+																													// SWAGMOVE
+		String babystepsTime = pointer.item(0).getAttributes().getNamedItem("time").getTextContent(); // Convert
+																										// to
+																										// double
+
+		// Timetracking
+		pointer = exercise.getElementsByTagName("timetracking");
+
+		Boolean timetracking = pointer.item(0).getAttributes().getNamedItem("value").getTextContent().equals("True");
+
+		// Medals... yeah we totally need it
+		pointer = exercise.getElementsByTagName("medals");
+
+		String bronzetime = pointer.item(0).getAttributes().getNamedItem("bronze").getTextContent(); // Convert
+																										// these
+																										// three
+		String silvertime = pointer.item(0).getAttributes().getNamedItem("silver").getTextContent();
+		String goldtime = pointer.item(0).getAttributes().getNamedItem("gold").getTextContent();
+
+		return new Exercise(name, id, description, referencedClasses, referencedTests, referencedFinishTests,
+				new ExerciseConfig(babysteps, Double.parseDouble(babystepsTime), timetracking,
+						Double.parseDouble(bronzetime), Double.parseDouble(silvertime), Double.parseDouble(goldtime))); // Done!
 	}
-	
-	
-	
+
 }
