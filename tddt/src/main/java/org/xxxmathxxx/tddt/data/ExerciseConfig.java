d58e51c9e2e628527198ab65e1c3eafc2b648fc7
Fabi <fweiss96@googlemail.com>
2016-06-28 19:10:20 +0200

    Another one

---
+++ tddt/src/main/java/org/xxxmathxxx/tddt/data/ExerciseConfig.java
@@ -0,0 +1,30 @@
+package org.xxxmathxxx.tddt.data;
+
+/**
+ * @author Fabian
+ * This class saves the Config of an exercise
+ */
+public class ExerciseConfig 
+{
+	public Boolean babystepsEnabeled;
+	public double babystepsTime; //In seks
+	
+	public Boolean timetrackingEnabled;
+	
+	public double bronzeTime; //In seks
+	public double silverTime;
+	public double goldTime;
+	
+	public ExerciseConfig(Boolean babystepsEnabled, double babystepstime, Boolean timetrackingEnabled, double bronzeTime, double silverTime, double goldTime)
+	{
+		this.babystepsEnabeled=babystepsEnabled;
+		this.babystepsTime= babystepstime;
+		
+		this.timetrackingEnabled=timetrackingEnabled;
+		
+		this.bronzeTime=bronzeTime;
+		this.silverTime=silverTime;
+		this.goldTime= goldTime;
+		
+	}
+}
