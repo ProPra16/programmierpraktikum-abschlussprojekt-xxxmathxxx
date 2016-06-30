25eb2be77977130543c10a8dd259ffea33895d90
Fabi <fweiss96@googlemail.com>
2016-06-30 16:39:49 +0200

    Added tests

--- tddt/src/test/java/org/xxxmathxxx/tddt/io/ExerciseReaderTest.java
+++ tddt/src/test/java/org/xxxmathxxx/tddt/io/ExerciseReaderTest.java
@@ -2,6 +2,7 @@ package org.xxxmathxxx.tddt.io;
 
 import org.junit.Test;
 import org.xxxmathxxx.tddt.data.Exercise;
+import org.xxxmathxxx.tddt.data.ExerciseCollection;
 
 import static org.junit.Assert.*;
 
@@ -13,7 +14,10 @@ public class ExerciseReaderTest {
 	@Test
 	public void constructorTest() {
 		ExerciseReader er = new ExerciseReader();
-		er.readAllExercises();
+		ExerciseCollection ex= er.readAllExercises();
+		
+		//ex.getExercise(1).print();
+		//ex.getExercise(0).print();
 		
 	//	Exercise test = er.readExercise("RomanNumbers.xml");
 
