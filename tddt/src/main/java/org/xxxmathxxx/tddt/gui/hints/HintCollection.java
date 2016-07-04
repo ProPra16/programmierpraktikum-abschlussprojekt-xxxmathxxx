5aa22c468bc270d37dd70f84d1921feb28535743
Fourier <Philipp.Spohr@hhu.de>
2016-07-04 10:15:22 +0200

    Adjusted Hints, removed KANT until he is scaled

--- tddt/src/main/java/org/xxxmathxxx/tddt/gui/hints/HintCollection.java
+++ tddt/src/main/java/org/xxxmathxxx/tddt/gui/hints/HintCollection.java
@@ -10,13 +10,14 @@ public class HintCollection {
 	 * The static collection of hints
 	 */
 	private static Hint[] hints = new Hint[]{
-			//new Hint("file:graphics/infobackgrounds/davinci.jpg","Did you know that TDD was invented by famous artist Leonardo DiCaprio as early as 1500? (Sadly his notes were lost and only recently rediscovered)"),
-			//new Hint("file:graphics/infobackgrounds/lol.jpg","Did you know that the client of a popular MOBA is not written using TDD (and thus is barely working at all)?"),
-			//new Hint("file:graphics/infobackgrounds/pkmnred.jpg","Did you know that a program which can be written in one day using TDD takes around one lightyear to be written without TDD?"),
-			//new Hint("file:graphics/infobackgrounds/nokor.jpg","Did you know that North Korea bans the public use of TDD due to the grave danger it poses to the political party?"),
-			//new Hint("file:graphics/infobackgrounds/stoneage.jpg","Did you know that during the Stone Age no software was written at all? Experts believe that this is due to the lack of TDD in this era."),
-			//new Hint("file:graphics/infobackgrounds/stoneage.jpg","Did you know that infamous SkyNet was developed using TDD?"),
-			new Hint("file:graphics/infobackgrounds/kantbackground.jpeg", "Did you know that in any particular theory of nature only so much real science can be encountered, as there is TDD therein?")
+			new Hint("file:graphics/infobackgrounds/davinci.jpg","Did you know that TDD was invented by famous artist Leonardo DiCaprio as early as 1500? (Sadly his notes were lost and only recently rediscovered)"),
+			new Hint("file:graphics/infobackgrounds/lol.jpg","Did you know that the client of a popular MOBA is not written using TDD (and thus is barely working at all)?"),
+			new Hint("file:graphics/infobackgrounds/pkmnred.jpg","Did you know that a program which can be written in one day using TDD takes around one lightyear to be written without TDD?"),
+			new Hint("file:graphics/infobackgrounds/nokor.jpg","Did you know that North Korea bans the public use of TDD due to the grave danger it poses to the political party?"),
+			new Hint("file:graphics/infobackgrounds/stoneage.jpg","Did you know that during the Stone Age no software was written at all? Experts believe that this is due to the lack of TDD in this era."),
+			new Hint("file:graphics/infobackgrounds/stoneage.jpg","Did you know that infamous SkyNet was developed using TDD?"),
+			//new Hint("file:graphics/infobackgrounds/kantbackground.jpeg", "Did you know that in any particular theory of nature only so much real science can be encountered, as there is TDD therein?")
+			//TODO: Kant needs scaling and source information
 	};
 	
 	/**Getter function returning a JavaFX Notification containing a random hint
