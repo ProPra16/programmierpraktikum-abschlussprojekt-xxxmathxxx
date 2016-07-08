a908e32b8285ea357caaaed8800f7c2cf0946264
Fabi <fweiss96@googlemail.com>
2016-07-08 13:25:46 +0200

    Added stateswitching

--- tddt/src/main/java/org/xxxmathxxx/tddt/gui/scenes/Editor.java
+++ tddt/src/main/java/org/xxxmathxxx/tddt/gui/scenes/Editor.java
@@ -8,6 +8,7 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
 
 /**
@@ -20,6 +21,13 @@ public class Editor extends Scene {
 	TestEditPane tep;
 	CodeEditPane cep;
 	
+	//Editstate
+	Byte state;
+	/**
+	 * state 0= test
+	 * state 1= code
+	 * state 2= refractor
+	 */
 	
 	//Teschebycheff's Tracker
 	Tracker tracker;
@@ -27,11 +35,12 @@ public class Editor extends Scene {
 	//Menus
 	Pane pane;
 	Button switchButton;
+	Label stateLabel;
 	
 	//LoadedExercise
 	Exercise ex;
 	
-	/**
+	/**fgcgh
 	 * Constructor
 	 * @param pane See Scene
 	 * @param ex Exercise to be loaded
@@ -40,6 +49,8 @@ public class Editor extends Scene {
 		super(pane);
 		this.ex=ex;
 		this.pane=pane;
+		
+		state=0;
 		
 		double xSize = pane.getPrefWidth();
 		double ySize = pane.getPrefHeight();
@@ -58,20 +69,24 @@ public class Editor extends Scene {
 		
 		cep.setVisible(false);
 		
-		//Test
+		//Button
 		switchButton= new Button("Switch!");
 		switchButton.setPrefSize(100, 50);
 		switchButton.relocate(xSize-200,ySize-150);
 		switchButton.addEventHandler(ActionEvent.ANY, new menuButtonHandler());
 		pane.getChildren().add(switchButton);
 		
-		
+		//Label
+		stateLabel= new Label("Teststage");
+		stateLabel.setPrefSize(100, 50);
+		stateLabel.relocate(10,ySize-150);
+		pane.getChildren().add(stateLabel);
 	}
 	
 	/**
 	 * Switches state between Test and Code Editor.
 	 */
-	public void switchState()
+	public void switchLabel()
 	{
 		if(tep.isActive())
 		{
@@ -91,12 +106,65 @@ public class Editor extends Scene {
 		}
 	}
 	
+	/**
+	 * Called when a stateswitch was requested by the User
+	 */
+	public void switchRequested()
+	{
+		switch(state)
+		{
+		case 0: //Switch to code
+			if(true) //TODO: Test if one test fails
+			{
+				switchLabel();
+				state=1;
+				updateStateLabel();
+			}
+			break;
+			
+		case 1: //Switch to refractor
+			if(true) //TODO: Test if code compiles and no test are failing
+			{
+				state=2;
+				updateStateLabel();
+			}
+			break;
+			
+		case 2: //Switch to test
+			switchLabel();
+			state=0;
+			updateStateLabel();
+			break;
+		}	
+	}
+	
+	/**
+	 * Updates StateLabel to currentState
+	 */
+	private void updateStateLabel()
+	{
+		switch(state)
+		{
+		case 0: 
+			stateLabel.setText("Teststage");
+			break;
+			
+		case 1: 
+			stateLabel.setText("CodeStage");
+			break;
+			
+		case 2: 
+			stateLabel.setText("Refractorstage");
+			break;
+		}	
+	}
+	
 	private final class menuButtonHandler implements EventHandler<ActionEvent>{
 
 		@Override
 		public void handle(ActionEvent event) {
 			if (event.getSource()==switchButton){
-				switchState();
+				switchRequested();
 			}
 		}
 	}
