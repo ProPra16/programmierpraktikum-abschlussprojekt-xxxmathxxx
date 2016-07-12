0d2f57cc22f5a37853fd5c58b9f1a85c940f6fb3
Fourier <Philipp.Spohr@hhu.de>
2016-07-12 14:39:45 +0200

    Rounded decimals in log

--- tddt/src/main/java/org/xxxmathxxx/tddt/tracking_analysis/AnalyzedStage.java
+++ tddt/src/main/java/org/xxxmathxxx/tddt/tracking_analysis/AnalyzedStage.java
@@ -1,5 +1,8 @@
 package org.xxxmathxxx.tddt.tracking_analysis;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 import org.xxxmathxxx.tddt.logging.TDDTLogManager;
 import org.xxxmathxxx.tddt.tracking.ActivityTracker;
 
@@ -27,7 +30,7 @@ public class AnalyzedStage {
 
 	public void log() {
 		TDDTLogManager.getInstance().logMessage("You used "+keystrokes+" keystrokes!");
-		TDDTLogManager.getInstance().logMessage("You spent "+time+" seconds in this phase!");
+		TDDTLogManager.getInstance().logMessage("You spent "+new BigDecimal(time).setScale(2, RoundingMode.HALF_UP)+" seconds in this phase!");
 		error.log();
 	}
 }
