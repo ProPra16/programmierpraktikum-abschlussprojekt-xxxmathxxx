59f58f9a88943ba1f20fd275bd7dac3e76d54927
Fabi <fweiss96@googlemail.com>
2016-07-12 15:46:51 +0200

    Added high-value Unittests

---
+++ tddt/src/test/java/org/xxxmathxxx/tddt/core/TDDTThreadTest.java
@@ -0,0 +1,40 @@
+package org.xxxmathxxx.tddt.core;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.xxxmathxxx.tddt.data.CodeStage;
+import org.xxxmathxxx.tddt.data.Exercise;
+import org.xxxmathxxx.tddt.gui.scenes.Editor;
+import org.xxxmathxxx.tddt.io.ExerciseReader;
+
+import javafx.scene.layout.Pane;
+
+public class TDDTThreadTest {
+
+	@Test
+	public void constructorTest() {
+		TDDTThread.getInstance().initialize();
+		TDDTThread.getInstance().beginExercise(ExerciseReader.readAllExercises().get(0));
+		
+		if(TDDTThread.getInstance() instanceof TDDTThread)
+		{
+			assertEquals(true, true);
+		}
+		else
+		{
+			assertEquals(true, false);
+		}
+	}
+	
+	@Test
+	public void trackingtest() {
+		TDDTThread.getInstance().initialize();
+		TDDTThread.getInstance().beginExercise(ExerciseReader.readAllExercises().get(0));
+		
+		assertEquals(TDDTThread.getInstance().trackerManager.getActiveTracker().keystrokes, 0);
+		assertEquals(TDDTThread.getInstance().trackerManager.atMap.get(CodeStage.CODE).getElapsedTime(), 0.0, 0.0);
+		assertEquals(TDDTThread.getInstance().trackerManager.atMap.get(CodeStage.REFACTOR).getElapsedTime(), 0.0, 0.0);
+		
+	}
+}
