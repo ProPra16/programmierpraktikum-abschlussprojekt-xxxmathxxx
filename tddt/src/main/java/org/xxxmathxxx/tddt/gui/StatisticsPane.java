6ff3259ead14ac063018df3fed0b256ea2f3c3ec
Tschebyscheff <Lukas.Wagner@uni-duesseldorf.de>
2016-07-12 22:30:07 +0200

    added basic informations in statisticpane

--- tddt/src/main/java/org/xxxmathxxx/tddt/gui/StatisticsPane.java
+++ tddt/src/main/java/org/xxxmathxxx/tddt/gui/StatisticsPane.java
@@ -1,6 +1,5 @@
 package org.xxxmathxxx.tddt.gui;
 import java.io.IOException;
-import java.util.ArrayList;
 
 import org.xxxmathxxx.tddt.core.TDDTThread;
 import org.xxxmathxxx.tddt.data.CodeStage;
@@ -15,6 +14,8 @@ import org.xxxmathxxx.tddt.tracking_analysis.ErrorType;
 
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.chart.BarChart;
@@ -26,19 +27,18 @@ import javafx.scene.chart.XYChart.Data;
 import javafx.scene.chart.XYChart.Series;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.MenuButton;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 
 /**
- * @author Tschebyscheff
+ * @author xxxMath2016xxx
  *
  */
 public class StatisticsPane extends Pane{  
 
 	private Profile profile;
 	private AnalyzedTrackingData analyzedTrackingData; 
-	private  ArrayList<  XYChart.Series<String,Integer> > seriesList; 
+
 	@FXML
 	private BarChart<String, Integer> barChart;
 	@FXML
@@ -50,26 +50,21 @@ public class StatisticsPane extends Pane{
 	@FXML
 	private PieChart pieChart;
 	@FXML
-	private MenuButton menuButton;
-	@FXML
 	private Button buttonBack;
-	@FXML
 	private ExerciseComboBox exPick;
     
-	
-   
 	public StatisticsPane(){
 		
 		this.profile = TDDTThread.getInstance().getUserProfile();
 		profileLabel = new Label();
 		profile.showNameInJavaFXLabel(profileLabel);
 		
+		
 		//Never ever delete some FXML stuff, it behaves different in combination with gradle
 		java.net.URL location = getClass().getResource("Stats.fxml");
 		FXMLLoader fxmlLoader = new FXMLLoader();
 		fxmlLoader.setLocation(location);
 		fxmlLoader.setController(this);
-		
 		
 		try {
 			//Never ever delete some FXML stuff, it behaves different in combination with gradle
@@ -85,34 +80,68 @@ public class StatisticsPane extends Pane{
 		}
 	}
 	
-	
-	
+	@SuppressWarnings("unchecked")
 	@FXML
 	private void initialize(){
 
-		
-		ArrayList<PieChart.Data> pieChartData = new ArrayList<PieChart.Data>();
-
 		if(analyzedTrackingData != null){
-			for ( CodeStage stage : analyzedTrackingData.anMap.keySet()){
-				AnalyzedStage data = analyzedTrackingData.anMap.get(stage);
-				for (ErrorType type : ErrorType.values()){ // loop over error types
-					Series<String,Integer> tmpSeries = new Series<String, Integer>();
-					tmpSeries.getData().add(new Data<String, Integer>(stage.toString(),data.error.getErrorCount(type)));
-					seriesList.add(tmpSeries);
-				}
-				pieChartData.add(new PieChart.Data(stage.toString(),data.time));
-			}
 			
-			xAxis.setLabel("Error");
-			yAxis.setLabel("Number");
-			barChart.getData().addAll(seriesList);
-	
+			AnalyzedStage stage1 = analyzedTrackingData.anMap.get(CodeStage.TEST);
+			AnalyzedStage stage2 = analyzedTrackingData.anMap.get(CodeStage.CODE);
+			AnalyzedStage stage3 = analyzedTrackingData.anMap.get(CodeStage.REFACTOR);
+
+			ObservableList<PieChart.Data> pieChartData =
+	                FXCollections.observableArrayList(
+	                new PieChart.Data("StageRed", (int) stage1.time ),
+	                new PieChart.Data("StageRefactor", (int) stage3.time ),
+	                new PieChart.Data("StageGreen", (int) stage2.time ));
+			
 			pieChart.getData().addAll(pieChartData);
 			pieChart.setTitle("Time");
+			
+			XYChart.Series<String, Integer> series1 = new Series<String, Integer>();
+			XYChart.Series<String, Integer> series2 = new Series<String, Integer>();
+			XYChart.Series<String, Integer> series3 = new Series<String, Integer>();
+			XYChart.Series<String, Integer> series4 = new Series<String, Integer>();
+			XYChart.Series<String, Integer> series5 = new Series<String, Integer>();
+			XYChart.Series<String, Integer> series6 = new Series<String, Integer>();
+			
+			series1.setName("notInitializedError");
+			series3.setName("cannotFindError");
+			series4.setName("returnTypeError");
+			series5.setName("expectedError");
+			series6.setName("semanticError");
+			series2.setName("syntaxError");
+			
+			series1.getData().add(new Data<String, Integer>("Stage Red", stage1.error.getErrorCount(ErrorType.NOT_INITIALIZED)));
+			series1.getData().add(new Data<String, Integer>("Stage Green", stage2.error.getErrorCount(ErrorType.NOT_INITIALIZED)));
+			series1.getData().add(new Data<String, Integer>("Stage Refactor", stage3.error.getErrorCount(ErrorType.NOT_INITIALIZED)));
+
+			series2.getData().add(new Data<String, Integer>("Stage Red", stage1.error.getErrorCount(ErrorType.SYNTAX)));
+			series2.getData().add(new Data<String, Integer>("Stage Green", stage2.error.getErrorCount(ErrorType.SYNTAX)));
+			series2.getData().add(new Data<String, Integer>("Stage Refactor", stage3.error.getErrorCount(ErrorType.SYNTAX)));
+			
+			series3.getData().add(new Data<String, Integer>("Stage Red", stage1.error.getErrorCount(ErrorType.CANNOT_FIND)));
+			series3.getData().add(new Data<String, Integer>("Stage Green", stage2.error.getErrorCount(ErrorType.CANNOT_FIND)));
+			series3.getData().add(new Data<String, Integer>("Stage Refactor", stage3.error.getErrorCount(ErrorType.CANNOT_FIND)));
+			
+			series4.getData().add(new Data<String, Integer>("Stage Red", stage1.error.getErrorCount(ErrorType.RETURN_TYPE)));
+			series4.getData().add(new Data<String, Integer>("Stage Green", stage2.error.getErrorCount(ErrorType.RETURN_TYPE)));
+			series4.getData().add(new Data<String, Integer>("Stage Refactor", stage3.error.getErrorCount(ErrorType.RETURN_TYPE)));
+			
+			series5.getData().add(new Data<String, Integer>("Stage Red", stage1.error.getErrorCount(ErrorType.EXPECTED_WRONG_TYPE)));
+			series5.getData().add(new Data<String, Integer>("Stage Green", stage2.error.getErrorCount(ErrorType.EXPECTED_WRONG_TYPE)));
+			series5.getData().add(new Data<String, Integer>("Stage Refactor", stage3.error.getErrorCount(ErrorType.EXPECTED_WRONG_TYPE)));
+			
+			series6.getData().add(new Data<String, Integer>("Stage Red", stage1.error.getErrorCount(ErrorType.SEMANTIC)));
+			series6.getData().add(new Data<String, Integer>("Stage Green", stage2.error.getErrorCount(ErrorType.SEMANTIC)));
+			series6.getData().add(new Data<String, Integer>("Stage Refactor", stage3.error.getErrorCount(ErrorType.SEMANTIC)));
+			
+			
+			barChart.getData().addAll(series1, series2, series3, series4, series5, series6);
+			xAxis.setLabel("Error");
+			yAxis.setLabel("Number");
 		}
-		
-		
 	}
 
 	ChangeListener<Exercise> changeListener = new ChangeListener<Exercise>(){
