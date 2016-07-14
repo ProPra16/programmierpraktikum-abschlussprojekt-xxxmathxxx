a2d052f37902c48d528bbcb64864f4c4f0aeb6ce
philipp <philipp.spohr@hhu.de>
2016-07-14 12:50:00 +0200

    JavaDoc-ed RestrictedTextField refs #54

--- tddt/src/main/java/org/xxxmathxxx/tddt/gui/RestrictedTextField.java
+++ tddt/src/main/java/org/xxxmathxxx/tddt/gui/RestrictedTextField.java
@@ -4,14 +4,26 @@ import java.util.regex.Pattern;
 
 import javafx.scene.control.TextField;
 
+/**An extension of the classical TextField that only allows alphanumerical input and is limited to 20 characters
+ * @author xxxMathxxx 2016
+ *
+ */
 public class RestrictedTextField extends TextField{
 	
-	private static Pattern p = Pattern.compile("[^a-zA-Z0-9]");
+	/**
+	 * The pattern (as RegEx) that is not allowed in this test field (note the negation)
+	 */
+	private static Pattern notAllowedPattern = Pattern.compile("[^a-zA-Z0-9]");
+	
+	/**
+	 * The maximum number of characters that can be inserted into this text field
+	 */
 	private static int maxSize = 20;
 
-    @Override public void replaceText(int start, int end, String text) {
+    @Override 
+    public void replaceText(int start, int end, String text) {
     	String oldText = getText();
-        if (!p.matcher(text).find()) {
+        if (!notAllowedPattern.matcher(text).find()) {
             super.replaceText(start, end, text);
         }
         if (getText().length() > maxSize){
@@ -21,7 +33,7 @@ public class RestrictedTextField extends TextField{
   
     @Override public void replaceSelection(String text) {
     	String oldText = getText();
-        if (!p.matcher(text).find()) {
+        if (!notAllowedPattern.matcher(text).find()) {
             super.replaceSelection(text);
         }
         if (getText().length() > maxSize){
