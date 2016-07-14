77a6b4a9dedd391231354540edea0d89d8fc61ec
Fourier <Philipp.Spohr@hhu.de>
2016-07-14 10:25:38 +0200

    closes #57

--- tddt/src/main/java/org/xxxmathxxx/tddt/gui/StatisticsPane.java
+++ tddt/src/main/java/org/xxxmathxxx/tddt/gui/StatisticsPane.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import org.xxxmathxxx.tddt.core.TDDTThread;
 import org.xxxmathxxx.tddt.data.CodeStage;
 import org.xxxmathxxx.tddt.data.Exercise;
-import org.xxxmathxxx.tddt.gui.WindowManager.MenuType;
 import org.xxxmathxxx.tddt.io.ExerciseReader;
 import org.xxxmathxxx.tddt.logging.TDDTLogManager;
 import org.xxxmathxxx.tddt.profile.Profile;
@@ -16,6 +15,8 @@ import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.chart.BarChart;
@@ -27,7 +28,9 @@ import javafx.scene.chart.XYChart.Data;
 import javafx.scene.chart.XYChart.Series;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 
 /**
@@ -48,10 +51,22 @@ public class StatisticsPane extends Pane{
 	@FXML
 	private Label profileLabel;
 	@FXML
+	private Label totalTimeLabel;
+	@FXML
+	private Label totalKeystrokesLabel;
+	@FXML
 	private PieChart pieChart;
 	@FXML
-	private Button buttonBack;
-	private ExerciseComboBox exPick;
+	private PieChart pieChartKeystrokes;
+	@FXML
+	private HBox topBar;
+	@FXML
+	private ImageView profileImageView;
+	
+	//non fxml elements
+	private ExerciseComboBox exercisePicker;
+	private MedalViewer medalViewer;
+	private Button back;
     
 	public StatisticsPane(){
 		
@@ -67,11 +82,25 @@ public class StatisticsPane extends Pane{
 		fxmlLoader.setController(this);
 		
 		try {
-			//Never ever delete some FXML stuff, it behaves different in combination with gradle
+			//Add non-fxml elements
 			AnchorPane anchorPane = fxmlLoader.load(location.openStream());
-			exPick = new ExerciseComboBox(ExerciseReader.readAllExercises().asObservableList());
-			exPick.getSelectionModel().selectedItemProperty().addListener(changeListener);
-			anchorPane.getChildren().add(exPick);
+			
+			exercisePicker = new ExerciseComboBox(ExerciseReader.readAllExercises().asObservableList());
+			exercisePicker.getSelectionModel().selectedItemProperty().addListener(changeListener);
+			exercisePicker.setPrefSize(196, 32);
+			exercisePicker.relocate(16, 16);
+			anchorPane.getChildren().add(exercisePicker);
+			
+			medalViewer = new MedalViewer();
+			topBar.getChildren().add(medalViewer);
+			
+			back = new Button("Back");
+			back.setPrefSize(150, 25);
+			back.relocate(800-475,600-90);
+			back.addEventHandler(ActionEvent.ANY, new menuButtonHandler());
+			anchorPane.getChildren().add(back);
+			
+			//Add fxml pane
 			getChildren().add(anchorPane);
 			
 		} catch (IOException e) {
@@ -82,13 +111,22 @@ public class StatisticsPane extends Pane{
 	
 	@FXML
 	private void initialize(){
-
+		
+		Profile profile = TDDTThread.getInstance().getUserProfile();
+		
+		profile.showImageInJavaFXImageView(profileImageView);
+		profile.showNameInJavaFXLabel(profileLabel);
+		
 		if(analyzedTrackingData != null){
 			
 			AnalyzedStage stage1 = analyzedTrackingData.anMap.get(CodeStage.TEST);
 			AnalyzedStage stage2 = analyzedTrackingData.anMap.get(CodeStage.CODE);
 			AnalyzedStage stage3 = analyzedTrackingData.anMap.get(CodeStage.REFACTOR);
 
+			int totalTime = (int)(stage1.time+stage2.time+stage3.time);
+			totalTimeLabel.setText("Total time elapsed: "+totalTime);
+			
+			pieChart.getData().clear();
 			ObservableList<PieChart.Data> pieChartData =
 	                FXCollections.observableArrayList(
 	                new PieChart.Data("StageRed", (int) stage1.time ),
@@ -98,6 +136,20 @@ public class StatisticsPane extends Pane{
 			pieChart.getData().addAll(pieChartData);
 			pieChart.setTitle("Time");
 			
+			int totalKeystrokes = stage1.getKeystrokes()+stage2.getKeystrokes()+stage3.getKeystrokes();
+			totalTimeLabel.setText("Total time elapsed: "+totalKeystrokes);
+			
+			pieChartKeystrokes.getData().clear();
+			pieChartData =
+	                FXCollections.observableArrayList(
+	                new PieChart.Data("StageRed", (int) stage1.getKeystrokes() ),
+	                new PieChart.Data("StageRefactor", (int) stage3.getKeystrokes() ),
+	                new PieChart.Data("StageGreen", (int) stage2.getKeystrokes() ));
+			
+			pieChartKeystrokes.getData().addAll(pieChartData);
+			pieChartKeystrokes.setTitle("Keystrokes");
+			
+			barChart.getData().clear();
 			for (ErrorType e : ErrorType.values()){
 				XYChart.Series<String, Integer> curSeries = new Series<String, Integer>();
 				curSeries.setName(e.toString());
@@ -120,6 +172,7 @@ public class StatisticsPane extends Pane{
 			if (newValue != null) {
 				try{
 				analyzedTrackingData = profile.profileStats.getTrackingData(newValue.id);
+				medalViewer.setMedals(newValue, TDDTThread.getInstance().getUserProfile());
 				initialize();
 				}
 				catch(NullPointerException e){
@@ -129,9 +182,18 @@ public class StatisticsPane extends Pane{
 		}
 	};
 	
-	@FXML
-	public void handleButtonBack(){
-		WindowManager.getInstance().showMenu(MenuType.EXISTING_PROFILE);
+	
+	/**
+	 * Handler for the different buttons
+	 */
+	private final class menuButtonHandler implements EventHandler<ActionEvent>{
+
+		@Override
+		public void handle(ActionEvent event) {
+			if (event.getSource() == back){
+				WindowManager.getInstance().showMenu(WindowManager.MenuType.EXISTING_PROFILE);
+			}
+		}
 	}
 	
 }
